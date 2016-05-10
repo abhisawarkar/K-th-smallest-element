b845378b15dada3246f4356f35dd713d7dabd411
Abhishek Sawarkar <sawarkar.ab@husky.neu.edu>
2016-05-09 22:14:10 -0700

    Update kth_smallest.py

--- kth_smallest.py
+++ kth_smallest.py
@@ -1,23 +1,23 @@
 def kthSmallest(arr,low,high,k):
 
 	if k>0 and k<=high-low+1:
-		pos=partition(arr,low,high)
+		pos=partition(arr,low,high)			# find position to split
 	
 		if pos-low == k-1:
 			return arr[pos]
 		if pos -low > k-1:
-			return kthSmallest(arr,low,pos-1,k)
-		return kthSmallest(arr,pos+1,high,k-(pos-low)+1)
+			return kthSmallest(arr,low,pos-1,k)		# check left side 
+		return kthSmallest(arr,pos+1,high,k-(pos-low)+1)	# check right side
 	
 
-def partition(arr,low,high):
+def partition(arr,low,high):		# Partition into two halves by pivot
 	wall = low-1
     	pivot = arr[high]     
     	for j in range(low , high):
         	if   arr[j] <= pivot:
-        	    wall += 1
-        	    arr[wall],arr[j] = arr[j],arr[wall]
+        	    wall += 1					# move wall
+        	    arr[wall],arr[j] = arr[j],arr[wall]		# Swap values less than pivot to sort rest of list
  
-    	arr[wall+1],arr[high] = arr[high],arr[wall+1]
+    	arr[wall+1],arr[high] = arr[high],arr[wall+1]		# Sawap to plcae pivot to front of wall
     	return wall+1
 
